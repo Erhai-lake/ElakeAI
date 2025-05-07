6f39b222c255321d674f30d16a8be7249065eb82
Erhai-lake <fuzixuan0714_0826@163.com>
2025-05-08 01:26:49 +0800

    refactor: 优化全局样式和布局
    - 将全局样式和滚动条样式从 App.vue 移动到 theme.less
    - 优化了样式结构，提高了代码的可维护性和可复用性
    - 通过使用变量和统一的样式规则，增强了主题的一致性

--- src/App.vue
+++ src/App.vue
@@ -158,40 +158,9 @@ export default {
 </template>
 
 <style lang="less">
-* {
-    margin: 0;
-    padding: 0;
-    transition: all 0.3s ease-in-out;
-}
-
 #app {
     width: 100%;
     height: 100vh;
     display: flex;
-}
-
-//全局滚动条
-::-webkit-scrollbar {
-    //垂直滚动条宽度
-    width: 10px;
-    //水平滚动条高度
-    height: 10px;
-}
-
-/* 滚动条轨道 */
-::-webkit-scrollbar-track {
-    background: var(--scrollbar-track-color);
-    border-radius: 4px;
-}
-
-/* 滚动条滑块 */
-::-webkit-scrollbar-thumb {
-    background: var(--scrollbar-thumb-color);
-    border-radius: 4px;
-}
-
-/* 滚动条滑块悬停状态 */
-::-webkit-scrollbar-thumb:hover {
-    background: var(--scrollbar-thumb-hover-color);
 }
 </style>
