31b4d60c4789804dc35a9d3e791680f8d8954678
Erhai-lake <fuzixuan0714_0826@163.com>
2025-06-21 19:44:31 +0800

    fix(components): 修复折叠面板组件的初始化问题
    - 在 FoldingPanel 组件中添加 watch 监听器，以响应 props 变化
    - 更新阿里云字体链接

--- src/components/FoldingPanel.vue
+++ src/components/FoldingPanel.vue
@@ -11,6 +11,14 @@ export default {
 			default: false
 		}
 	},
+	watch: {
+		Height(newVal) {
+			this.contentHeight = newVal
+		},
+		is(newVal) {
+			this.isExpand = newVal
+		}
+	},
 	data() {
 		return {
 			contentHeight: this.Height,
