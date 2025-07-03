bfd7247688523580204f2dc509f040342a9b9470
Erhai-lake <fuzixuan0714_0826@163.com>
2025-07-03 15:36:29 +0800

    feat(plugin): 添加第三方示例插件
    - 新增 thirdPartyExample 插件，用于演示插件的安装和加载过程
    - 实现了插件的安装、注册、加载和卸载的生命周期方法
    - 提供了一个示例插件的配置文件 plugin.json

---
+++ electron/plugins/thirdPartyExample/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+	onInstall(ctx) {
+		console.log("插件安装成功", ctx)
+	},
+	onRegister(ctx) {
+		console.log("插件注册成功", ctx)
+	},
+	onLoad(ctx) {
+		console.log("插件加载完成", ctx)
+	},
+	onUnload() {
+		console.log("插件已卸载")
+	}
+}
